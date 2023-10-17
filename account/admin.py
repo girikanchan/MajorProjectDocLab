c4181d05bf4e82cd01c20e9741eac9437968a325
girikanchan <akira.giri2001@gmail.com>
2023-10-17 19:18:44 +0530

    admin page code

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from account.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+class UserModelAdmin(BaseUserAdmin):
+  # The fields to be used in displaying the User model.
+  # These override the definitions on the base UserModelAdmin
+  # that reference specific fields on auth.User.
+  list_display = ('id', 'email', 'name','date_of_birth', 'gender', 'occupation', 'contno', 'id_type', 'id_issue', 'id_number', 'issue_state', 'issue_date', 'add_type', 'address', 'nation', 'state', 'district', 'pincode', 'is_admin')
+  list_filter = ('is_admin',)
+  fieldsets = (
+      ('User Credentials', {'fields': ('email', 'password')}),
+      ('Personal info', {'fields': ('name', 'date_of_birth', 'gender', 'occupation', 'contno', 'id_type', 'id_issue', 'id_number', 'issue_state', 'issue_date', 'add_type', 'address', 'nation', 'state', 'district', 'pincode')}),
+      ('Permissions', {'fields': ('is_admin',)}),
+  )
+  # add_fieldsets is not a standard ModelAdmin attribute. UserModelAdmin
+  # overrides get_fieldsets to use this attribute when creating a user.
+  add_fieldsets = (
+      (None, {
+          'classes': ('wide',),
+          'fields': ('email', 'name', 'date_of_birth', 'gender', 'occupation', 'contno', 'id_type', 'id_issue', 'id_number', 'issue_state', 'issue_date', 'add_type', 'address', 'nation', 'state', 'district', 'pincode', 'password1', 'password2'),
+      }),
+  )
+  search_fields = ('email',)
+  ordering = ('email', 'id')
+  filter_horizontal = ()
+
+
+# Now register the new UserModelAdmin...
+admin.site.register(User, UserModelAdmin)
